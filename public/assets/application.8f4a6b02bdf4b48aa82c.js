3f983062c7569fb43d23dc5e5517a679d2bd3b40
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2024-02-26 14:12:47 -0400

    Update plant for functionality.

--- public/assets/application.8f4a6b02bdf4b48aa82c.js
+++ public/assets/application.8f4a6b02bdf4b48aa82c.js
@@ -102,7 +102,7 @@ eval("/*!\n * Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com
 /******/ 		        // webpack-livereload-plugin
 /******/ 		        (function() {
 /******/ 		          if (typeof window === "undefined") { return };
-/******/ 		          var id = "webpack-livereload-plugin-script-0caaebd14d52e4aa";
+/******/ 		          var id = "webpack-livereload-plugin-script-4b2b778dfd3e7775";
 /******/ 		          if (document.getElementById(id)) { return; }
 /******/ 		          var el = document.createElement("script");
 /******/ 		          el.id = id;
