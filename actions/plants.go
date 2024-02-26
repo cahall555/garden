9e1921ce6f7c1f49f596425182d21ae1501f7cc3
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2024-02-26 14:12:47 -0400

    Update plant for functionality.

--- actions/plants.go
+++ actions/plants.go
@@ -64,7 +64,8 @@ func PlantsNew(c buffalo.Context) error {
 		c.Flash().Add("error", "Plant form parsing error")
 		return c.Redirect(301, "/")
 	}
-	
+
+
 	pts := c.Request().FormValue("Tags")
 	newTags := strings.Split(pts, ",")
 	for _, nt := range newTags {
@@ -116,3 +117,139 @@ func PlantsNew(c buffalo.Context) error {
 	return c.Redirect(301, fmt.Sprintf("/plants/%s", plant.ID))
 }
 
+func PlantsUpdate(c buffalo.Context) error {
+	tx := c.Value("tx").(*pop.Connection)
+	plant := models.Plant{}
+	plantID := c.Param("id")
+	c.Logger().Info("Plant id: ", plantID)
+
+	err := tx.Eager("PlantTags").Find(&plant, plantID)
+   	if err != nil {
+		c.Logger().Error("Plant tags: ", plant.PlantTags)
+		return c.Redirect(302, "/")
+    	}
+    
+    	var tags []string
+    	for _, tag := range plant.PlantTags {
+        	tags = append(tags, tag.Name)
+    	}
+    	tagsStr := strings.Join(tags, ", ")
+
+	gardenId := &plant.GardenID
+	c.Logger().Info("Garden id: ", gardenId)
+    	c.Set("gardenId", gardenId)
+
+	gardens := &models.Gardens{}
+	err = tx.All(gardens)
+	if err != nil {
+		return c.Redirect(302, "/")
+	}
+	c.Set("gardens", gardens)
+
+	err = tx.Eager().Find(&plant, plantID)
+	if err != nil {
+		c.Flash().Add("warning", "Plant not found")
+		c.Redirect(301, "/")
+		c.Logger().Debug(plantID)
+		c.Logger().Error("Plant not found: ", err)
+	}
+	c.Set("tagsStr", tagsStr)
+	c.Set("plant", plant)
+	return c.Render(http.StatusOK, r.HTML("plants/update.html"))
+}
+
+func PlantsEdit(c buffalo.Context) error {
+	tx := c.Value("tx").(*pop.Connection)
+	plant := &models.Plant{}
+	if err := tx.Find(plant, c.Param("plantId")); err != nil {
+		return err
+	}
+	
+	err := c.Bind(plant)
+	if err != nil {
+		c.Flash().Add("warning", "Plant form binding error")
+		return c.Redirect(301, "/")
+	}
+
+	err = c.Request().ParseForm()
+	if err != nil {
+		c.Flash().Add("error", "Plant form parsing error")
+		return c.Redirect(301, "/")
+	}
+
+
+	pts := c.Request().FormValue("Tags")
+	newTags := strings.Split(pts, ",")
+	c.Logger().Info("New tags: ", newTags)
+	for _, nt := range newTags {
+		nt = strings.TrimSpace(nt)
+
+	t := &models.Tag{}
+	err = tx.Where("name = ?", nt).Last(t)
+	c.Logger().Info("Tag: ", t)
+	if err != nil {
+		if errors.Cause(err) == sql.ErrNoRows {
+			t.Name = nt
+			err2 := tx.Create(t)
+			if err2 != nil {
+				log.Fatal(err2)
+				c.Flash().Add("error", "Tags creation error")
+			}
+			} else {
+				log.Fatal(err)
+				continue
+			}
+		}
+		plant.PlantTags = append(plant.PlantTags, *t)
+
+		//If a new tag is added, a new relationship is created on the plants_tags table
+		var plantID = plant.ID
+		var tagID = t.ID
+
+		var existingRelationship models.PlantsTag
+		err := tx.Where("plant_id = ? AND tag_id = ?", plantID, tagID).First(&existingRelationship)
+
+		if err != nil {
+    			if errors.Cause(err) == sql.ErrNoRows {
+        			newRelationship := &models.PlantsTag{
+            				PlantID: plantID,
+            				TagID:   tagID,
+        	}
+
+        		verrs, err := tx.ValidateAndCreate(newRelationship)
+        		if err != nil {
+            			return c.Redirect(301, "/")
+        		}
+        		if verrs.HasAny() {
+            			c.Flash().Add("warning", "Relationship validation error")
+	    			c.Set("plant", plant)
+	    			c.Set("errors", verrs)
+	    			c.Logger().Error("Validation errors: ", verrs)
+	    			return c.Render(422, r.HTML("plants/update.html"))
+
+    			} else {
+        			c.Logger().Error("Relationship error: ", err)
+    			}
+		} else {
+			c.Logger().Info("Relationship already exists")
+		}
+		}
+	}
+
+	verrs, err := tx.Eager().ValidateAndUpdate(plant)
+	if err != nil {
+		c.Logger().Error("Plant update error: ", err)
+		return c.Redirect(301, "/")
+	}
+
+	if verrs.HasAny() {
+		c.Flash().Add("warning", "Plant validation error")
+		c.Set("plant", plant)
+		c.Set("errors", verrs)
+		c.Logger().Error("Validation errors: ", verrs)
+		return c.Render(422, r.HTML("plants/update.html"))
+	}
+
+	c.Flash().Add("success", "Plant updated")
+	return c.Redirect(301, fmt.Sprintf("/plants/%s", plant.ID))
+}
