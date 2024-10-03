ef07ad4386d6776d503b46cbb03535a5a2cd0832
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2024-10-03 12:23:09 -0400

    chore: add github workflow for flutter tests.

--- public/assets/application.61e51433d6ef0c350523.js
+++ public/assets/application.61e51433d6ef0c350523.js
@@ -102,7 +102,7 @@ eval("/*!\n * Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com
 /******/ 		        // webpack-livereload-plugin
 /******/ 		        (function() {
 /******/ 		          if (typeof window === "undefined") { return };
-/******/ 		          var id = "webpack-livereload-plugin-script-4b563be6f956efb6";
+/******/ 		          var id = "webpack-livereload-plugin-script-bbc2de35d7742cf3";
 /******/ 		          if (document.getElementById(id)) { return; }
 /******/ 		          var el = document.createElement("script");
 /******/ 		          el.id = id;
